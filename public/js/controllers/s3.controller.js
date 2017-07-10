ca621ac4ba5f53243060a9f012910eeeb79baf8e
yafiimo <mohammed.yafii@hotmail.co.uk>
2017-07-10 15:03:21 +0100

    upload multiple images

--- public/js/controllers/s3.controller.js
+++ public/js/controllers/s3.controller.js
@@ -2,30 +2,26 @@ function S3Controller(S3Factory) {
   var controller = this;
 
   controller.uploadImages = function() {
-    controller.imagesArray = controller.images.map(function(image) {
-      return {
-        name: image.name,
-        type: image.type
-      };
+    controller.images.forEach(function(image) {
+      S3Factory.getSignedRequests(image).then(
+        function success(success) {
+          console.log('Successfully got signed requests', success.data);
+          uploadToBucket(image, success.data.signedRequest);
+        },
+        function error(error) {
+          console.warn('Error getting signed requests', error);
+        }
+      );
     });
-    S3Factory.getSignedRequests(controller.imagesArray[0]).then(
-      function success(success) {
-        console.log('Successfully got signed requests', success.data);
-        uploadToBucket(controller.images[0], success.data.signedRequest, success.data.url);
-      },
-      function error(error) {
-        console.warn('Error getting signed requests', error);
-      }
-    );
   };
 
   function uploadToBucket(file, signedRequest) {
     // Here we make the XHR request to s3 to upload the images
-    console.log('helooooo', file);
+    console.log(signedRequest);
     var xhr = new XMLHttpRequest();
     xhr.open('PUT', signedRequest);
     xhr.send(file);
-    xhr.onreadystatechange = () => {
+    xhr.onreadystatechange = function() {
       if(xhr.readyState === 4){
         if(xhr.status === 200){
           controller.background = `https://s3.amazonaws.com/wedding-rsvp-app-photos/${file.name}`;
