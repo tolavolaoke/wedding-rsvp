ce5676fc933233330af7a1fe387884b437a8b07c
yafiimo <mohammed.yafii@hotmail.co.uk>
2017-07-16 01:29:55 +0100

    previw showing in browser

--- public/js/controllers/s3.controller.js
+++ public/js/controllers/s3.controller.js
@@ -1,51 +1,58 @@
 function S3Controller(S3Factory) {
   var controller = this;
 
-  // Get a signed request from aws s3 using the aws-sdk in the back-end
   controller.uploadImages = function() {
-    controller.images.forEach(function(file) {
-      S3Factory.getSignedRequests(file).then(
-        function success(success) {
-          const signedRequest = success.data.signedRequest;
-          file.url = success.data.url;
-
-          uploadToBucket(file, signedRequest).then(
-            function(url) {
-              console.log('url from promise return', url);
-              console.log(controller.images);
-            }).catch(
-              function(error) {
-                console.warn(error);
-              });
-        },
-        function error(error) {
-          console.warn('Error getting signed requests', error);
-        }
-      );
+    var requestPromises = [];
+    controller.images.forEach(function(file, index) {
+      controller.previews.push(`preview${index}`);
+      requestPromises.push(uploadToS3(file));
     });
+    Promise.all(requestPromises).then(
+      function(urls) {
+        console.log('uploaded images:', urls);
+        for(var i = 0; i < urls.length; i++) {
+          document.getElementById(`preview${i}`).src = urls[i];          
+        }
+      }).catch(
+      function(error) {
+        console.log(error);
+      });
   };
 
+  // Get a signed request from aws s3 using the aws-sdk in the back-end
   // Using the signed request returned from the back-end, make XHR request to s3 to upload the files
-  function uploadToBucket(file, signedRequest) {
+  function uploadToS3(file) {
     return new Promise(function(resolve, reject) {
-      var xhr = new XMLHttpRequest();
-      xhr.open('PUT', signedRequest);
-      xhr.onload = function() {
-        if(xhr.status === 200){
-          resolve(file.url);
-        } else {
-          reject(xhr.statusText);
+      S3Factory.getSignedRequests(file).then(
+        function success(success) {
+          const url = success.data.url;
+          const signedRequest = success.data.signedRequest;
+          console.log(signedRequest);
+
+          var xhr = new XMLHttpRequest();
+          xhr.open('PUT', signedRequest);
+          xhr.onload = function() {
+            if(xhr.status === 200){
+              resolve(url);
+            } else {
+              reject(xhr.statusText);
+            }
+          };
+          xhr.onerror = function() {
+            reject(xhr.statusText);
+          };
+          xhr.send(file);
+        },
+        function error(error) {
+          reject(error);
         }
-      };
-      xhr.onerror = function() {
-        reject(xhr.statusText);
-      };
-      xhr.send(file);
+        );
     });
   }
 
 
   function init() {
+    controller.previews = [];
   }
   init();
 
